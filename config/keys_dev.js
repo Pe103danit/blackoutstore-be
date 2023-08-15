2ddd76590fb8e6bb529126cb516b66dd79a2d28d
miegami <bohdanadm1n@gmail.com>
2023-08-15 14:42:22 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    `mongodb+srv://pe103danit:EqUKzSF98U2mGsK8@cluster0.nqdctup.mongodb.net/?retryWrites=true&w=majority`,
+  secretOrKey: 'random very very secret string',
+};
